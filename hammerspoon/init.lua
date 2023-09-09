34c3be307dcc056953756b46d7dc3383a97abe71
Exr0n <howdy@exr0n.com>
2023-09-09 01:52:37 -0700

    seems viable; mvp is plaintext and png. maybe html?

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,37 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "r", function()
+  hs.reload()
 end)
+
+function dump(o) -- https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
+  if type(o) == 'table' then
+     local s = '{ '
+     for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '['..k..'] = ' .. dump(v) .. ','
+     end
+     return s .. '} '
+  else
+     return tostring(o)
+  end
+end
+
+function keys(o)
+  if type(o) == 'table' then
+    local s = '{ '
+    for k,v in pairs(o) do
+      if type(k) ~= 'number' then k = '"'..k..'"' end -- add quotes to numeric keys
+      s = s ..'['..k..'], '
+    end
+    return s .. '} '
+  else
+    return tostring(o)
+  end
+end
+
+burrows_clipboard_watcher = hs.pasteboard.watcher.new(function ()
+  print("clipboard clipped!")
+  print(hs.pasteboard.getContents())
+  print(dump(hs.pasteboard.pasteboardTypes()))
+  print(keys(hs.pasteboard.readAllData()))
+end)
+-- print("is it running?", burrows_clipboard_watcher:running())
