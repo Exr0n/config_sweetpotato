b0783632841e2d6d258c85589a35131de7fd659d
Exr0n <mail@exr0n.com>
2024-07-23 15:25:38 -0400

    yabairc; and switch window/space don't use jq on query

--- skhd/switch_to_window_or_space.sh
+++ skhd/switch_to_window_or_space.sh
@@ -1,10 +1,7 @@
 DIRPATH="$HOME/.caches/yabai_custom/window_id_"
 # osascript -e "display notification \"checking file $DIRPATH$1 with $(cat "$DIRPATH$1")\" with title \"switch window or space\""
 if [[ -s "$DIRPATH$1" ]]; then
-    # osascript -e "display notification \"$(cat "$DIRPATH$1") in $(yabai -m query --windows | jq -r '.[].id')\" with title \"switch window or space\""
-    if echo "$( yabai -m query --windows | jq -r '.[].id')" | grep -q "$(cat "$DIRPATH$1")"; then
-        # osascript -e 'display notification "window found" with title "switch window or space"'
-        yabai -m window --focus $(cat "$DIRPATH$1")
+    if yabai -m window --focus $(cat "$DIRPATH$1"); then 
     else
         # osascript -e 'display notification "window not found" with title "switch window or space"'
         rm -f "$DIRPATH$1"
