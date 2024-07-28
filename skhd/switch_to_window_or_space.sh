f4477314aaf91c10d169a831ba938b470adc3d09
Exr0n <mail@exr0n.com>
2024-07-28 18:09:52 -0400

    some yabai and karabiner tuning

--- skhd/switch_to_window_or_space.sh
+++ skhd/switch_to_window_or_space.sh
@@ -2,6 +2,7 @@ DIRPATH="$HOME/.caches/yabai_custom/window_id_"
 # osascript -e "display notification \"checking file $DIRPATH$1 with $(cat "$DIRPATH$1")\" with title \"switch window or space\""
 if [[ -s "$DIRPATH$1" ]]; then
     if yabai -m window --focus $(cat "$DIRPATH$1"); then 
+        echo 'yay'
     else
         # osascript -e 'display notification "window not found" with title "switch window or space"'
         rm -f "$DIRPATH$1"
