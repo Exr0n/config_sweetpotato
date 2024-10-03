bd1c0948c0587b685e64fea9c178bbc9be765d3f
Exr0n <mail@exr0n.com>
2024-10-03 15:34:27 -0700

    subdirectory of paths for session-based window tags

--- skhd/set_window_mark.sh
+++ skhd/set_window_mark.sh
@@ -1,10 +1,15 @@
-DIRPATH="$HOME/.caches/yabai_custom/window_id_"
+SESSION_PATH="$HOME/.caches/yabai_custom/session_name"
+SESSION_NAME="$(< "$SESSION_PATH")"
+
+DIRPATH="$HOME/.caches/yabai_custom/sessions/$SESSION_NAME/window_id_"
+
 # rm -f "$DIRPATH$1"
 CUR_ID="$(yabai -m query --windows --window | jq -r '.id')"
 if [[ $(< "$DIRPATH$1") == "$CUR_ID" ]]; then
-    osascript -e "display notification \"untagged window $1\" with title \"switch window or space\""
+    osascript -e "display notification \"$SESSION_NAME: unset $1\" with title \"switch window or space\""
     rm -f "$DIRPATH$1"
 else
-    osascript -e "display notification \"tagged window $1\" with title \"switch window or space\""
+    osascript -e "display notification \"$SESSION_NAME: set $1\" with title \"switch window or space\""
+    mkdir -p "$DIRPATH"
     echo "$CUR_ID" > "$DIRPATH$1"
 fi
--- skhd/switch_to_window_or_space.sh
+++ skhd/switch_to_window_or_space.sh
@@ -1,4 +1,7 @@
-DIRPATH="$HOME/.caches/yabai_custom/window_id_"
+SESSION_PATH="$HOME/.caches/yabai_custom/session_name"
+SESSION_NAME="$(< "$SESSION_PATH")"
+
+DIRPATH="$HOME/.caches/yabai_custom/sessions/$SESSION_NAME/window_id_"
 # osascript -e "display notification \"checking file $DIRPATH$1 with $(cat "$DIRPATH$1")\" with title \"switch window or space\""
 if [[ -s "$DIRPATH$1" ]]; then
     if yabai -m window --focus $(cat "$DIRPATH$1"); then 
